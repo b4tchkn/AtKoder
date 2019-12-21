afdd3dacf3aa3fdcd0cb1427588cc09e4cfad0c9
batch <baaaakkrad@gmail.com>
2019-12-21 14:58:45 +0900

    [add]ShiftOnly.kt

--- src/CheatSheet.kt
+++ src/CheatSheet.kt
@@ -1,6 +1,7 @@
 import java.util.*
 
 fun main() {
+    // https://qiita.com/naoppy/items/e9064b297efae1a89642#n%E5%80%8B%E3%81%AE%E6%95%B0%E5%AD%97%E3%81%8C%E4%B8%80%E8%A1%8C%E3%81%AB%E3%81%82%E3%82%8B%E5%85%A5%E5%8A%9B
     // n行の入力を受け付ける
 //    val n = readLine()!!.toInt()
 //    val d = (1..n).map {
